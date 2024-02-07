504750a677b49a4ad6cc53ec5c9c3006f3e897c8
CarlEvans1 <carltonokoth94@gmail.com>
2024-02-07 15:29:51 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -2,21 +2,39 @@
 """class BaseModel that defines all common attributes/methods for other classes
 
 """
-
-
 import uuid
 import datetime
 
 class BaseModel:
     """Base class for all coming subclasses"""
 
+    def __init__(self, *args, **kwargs):     
+
+        if kwargs:
+            for key, value in kwargs.items():
+                 if key == 'created_at' or key == 'updated_at':
+                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                 elif key != '__class__':
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.datetime.now()
+            if 'updated_at' not in kwargs:     
+                self.updated_at = datetime.datetime.now()
+
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+
     def __init__(self, my_num=None, name=None):
-        '''initialize instance attributes'''
-        self.my_number = my_num
-        self.name = name
-        self.updated_at = datetime.datetime.now()
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
+         '''initialize instance attributes'''
+         self.my_number = my_num
+         self.name = name
+         self.updated_at = datetime.datetime.now()
+         self.id = str(uuid.uuid4())
+         self.created_at = datetime.datetime.now()
 
     def save(self):
         '''updates time of an instance'''
