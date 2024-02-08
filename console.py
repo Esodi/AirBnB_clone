661c4120cd36575b37b5ca955b4a8e00e6354e62
CarlEvans1 <carltonokoth94@gmail.com>
2024-02-08 16:16:27 +0300

    Command line interpreter

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""HBNB console"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """This is command line interpreter"""
+    prompt = "(hbnb)"
+
+    def do_quit(self, argument):
+        """Exit console"""
+        return True
+
+    def do_EOF(self, argument):
+        """Exit console on EOL"""
+        print()
+        return True
+
+    def help_quit(self):
+        print("Quit command to exit the program")
+
+    def help_EOL(self):
+        print("Exit console on EOL")
+
+if __name__ == '__main__':
+        HBNBCommand().cmdloop()
